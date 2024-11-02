4ddd5e9c1a772054f0a0df239848386c9ce7ad73
ronaldoski <119084578+ronaldoski@users.noreply.github.com>
2024-11-03 00:12:17 +0100

    first commit

---
+++ t1/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Home from "./composant/Home";
+function App() {
+
+  const Handleclick =(index:number) =>{
+
+    console.log(index);
+  }
+  return (
+    <div className="App">
+      <header>
+      </header>
+<nav>
+
+
+<Home nom= {["yo","yo"]} onClick={Handleclick}/>
+
+
+</nav>
+
+    </div>
+  );
+}
+
+export default App;
